f86dbd6fdbcd4b94cea7636b178e5b3112e65bd6
Wanjiru Catherine <catherinemunene802@gmail.com>
2019-12-12 15:04:55 +0300

    Added dice drawable resources

--- app/src/main/java/com/shirucodes/diceroller/MainActivity.kt
+++ app/src/main/java/com/shirucodes/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -20,16 +21,28 @@ class MainActivity : AppCompatActivity() {
         rollButton.setText("Let's Roll")
 
         rollButton.setOnClickListener{
+            Toast.makeText(this, "Button clicked",Toast.LENGTH_LONG).show()
             rollDice()
 
         }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice)
 
-        val random = Random().nextInt(6) + 1
+        val randomInt = Random().nextInt(6) + 1
 
-        resultText.text = random.toString()
+        val drawableResource = when (randomInt) {
+
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+
+            else -> R.drawable.dice_6
+        }
+
+       // resultText.text = random.toString()
     }
 }
